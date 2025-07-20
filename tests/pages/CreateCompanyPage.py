5cd7d28a34b7740f04a42dd745008711db6d8e3d
JF-Pedra <jfpedrazag@unal.edu.co>
2025-07-20 13:16:58 -0500

    CreateCompanyPage.py created

---
+++ tests/pages/CreateCompanyPage.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.common.by import By
+
+from tests.pages.BasePage import BasePage
+
+class CreateCompanyPage(BasePage):
+
+    URL = 'http://localhost:5000/create-company'
+    COMPANY_NAME_FIELD = (By.ID, 'name')
+    COMPANY_TYPE_DROPDOWN = (By.ID, 'type')
+    COMPANY_EMAIL_FIELD = (By.ID, 'email')
+    CREATE_COMPANY_BTN = (By.CSS_SELECTOR, 'button.btn-success')
+
+
+    def enter_company_name(self, name):
+        self.enter_text(self.COMPANY_NAME_FIELD, name)
+
+    def select_company_type(self, type):
+        self.select_by_value(self.COMPANY_TYPE_DROPDOWN, type)
+
+    def enter_company_email(self, email):
+        self.enter_text(self.COMPANY_EMAIL_FIELD, email)
+
+    def click_create_company_btn(self):
+        self.click(self.CREATE_COMPANY_BTN)
+
+
+
