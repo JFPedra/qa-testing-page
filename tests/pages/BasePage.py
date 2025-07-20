8272f554de93d43982f6bacdf0fcedb3a473d518
JF-Pedra <jfpedrazag@unal.edu.co>
2025-07-20 13:15:59 -0500

    New functions for Base page

--- tests/pages/BasePage.py
+++ tests/pages/BasePage.py
@@ -1,4 +1,4 @@
-
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -8,19 +8,34 @@ class BasePage():
         self.driver = driver
         self.wait = WebDriverWait(self.driver, 10)
 
+    def wait_until_url_is(self, url):
+        self.wait.until(EC.url_to_be(url))
+
+    def get_page_title(self):
+        return self.driver.title
+
+    def get_element(self, locator):
+        return self.wait.until(EC.presence_of_element_located(locator))
+
     def navigate_to(self, url):
         self.driver.get(url)
 
     def click(self, locator):
-        element = self.driver.find_element(locator)
+        element = self.get_element(locator)
         self.wait.until(EC.element_to_be_clickable(element))
+        element.click()
 
     def get_text(self, locator):
-        self.wait.until(EC.presence_of_element_located(locator))
-        return self.driver.find_element(*locator).text
+        return self.get_element(locator).text
 
     def get_elements(self, locator):
         self.wait.until(EC.presence_of_all_elements_located(locator))
         return self.driver.find_elements(*locator)
 
+    def enter_text(self, locator, text):
+        self.get_element(locator).send_keys(text)
+
+    def select_by_value(self, select_locator, value):
+        element = self.get_element(select_locator)
+        Select(element).select_by_value(value)
 
